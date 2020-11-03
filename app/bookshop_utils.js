812698336b22909c8ff769249ffa615bf6627f8b
Leila <leiladossantos@yahoo.com>
2020-11-03 17:56:38 +0000

    changes the last changes

--- app/bookshop_utils.js
+++ app/bookshop_utils.js
@@ -1,49 +1,41 @@
 function addVAT(price) {
-    return price + (0.2 * price);
+  return price + 0.2 * price;
 }
 
-function getFullName (name, surname){
-
-    return (name + ' ' + surname);
+function getFullName(name, surname) {
+  return name + " " + surname;
 }
 
-function makeHalfPrice (price) {
-return price / 2;
-
-
+function makeHalfPrice(price) {
+  return price / 2;
 }
 
-function countBooks (books) {
-return books.length
+function countBooks(books) {
+  return books.length;
 }
 
-function isInStock (book) {
-    if (book.quantity >= 1) {
-        return true
-    } else {
-        return false;
-    }
+function isInStock(book) {
+  if (book.quantity >= 1) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-function calculateVAT (price) {
-    return price * 0.20
+function calculateVAT(price) {
+  return price * 0.2;
 }
 
-
-function getTotalOrderPrice (price, quantity) {
-    // return price * quantity * 1.20;
-    return addVAT(price*quantity);
+function getTotalOrderPrice(price, quantity) {
+  // return price * quantity * 1.20;
+  return addVAT(price * quantity);
 }
-
 
 module.exports = {
-    addVAT,
-    getFullName,
-    makeHalfPrice,
-    countBooks,
-    isInStock,
-    getTotalOrderPrice
-
+  addVAT,
+  getFullName,
+  makeHalfPrice,
+  countBooks,
+  isInStock,
+  getTotalOrderPrice,
 };
-
-
